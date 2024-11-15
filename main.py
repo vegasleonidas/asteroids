f9fd471db225b458fdcac9693f22dd31a6c510f8
vegasleonidas <lhlewis@gmail.com>
2024-11-15 12:54:47 -0800

    first commit of asteroids project

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+   pygame.init()
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   while True:
+      for event in pygame.event.get():
+    	   if event.type == pygame.QUIT:
+            return
+      main_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+      main_surface.fill(pygame.Color('black'))
+      pygame.display.flip()	
+   print("Starting asteroids!")
+   print(f"Screen width: {SCREEN_WIDTH}")
+   print(f"Screen height: {SCREEN_HEIGHT}")	
+
+if __name__ == "__main__":
+    main()
+
